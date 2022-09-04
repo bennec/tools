df66059952bd87fee0d69ffb9ce2785bda55a776
Christophe Benne <christophe.benne@gmail.com>
2022-09-04 19:11:16 +0200

    Version basique du script d'import d'images brutes 5DM3.

--- sources/5DM3/bash/import-raw-pictures.sh
+++ sources/5DM3/bash/import-raw-pictures.sh
@@ -39,7 +39,59 @@
 #        les images sont automatiquement classées dans une arborescence de
 #        répertoire <YEAR>/<MONTH>/<DAY>.
 #
+# Ceci est une première version très basique du script d'import. Le script
+# prend deux arguments : le répertoire où est montée la carte de l'appareil
+# photo et le répertoire où les photos doivent être copiées. Toutes les
+# photo du répertoire $1/DCIM/100EOS5D sont copiées dans
+# $2/<YEAR>/<MONTH>/<DAY> sauf si le fichier existe déjà. <YEAR>, <MONTH>
+# et <DAY> sont l'année, le mois et le jour où la photo a été prise, ils
+# sont déduits des données EXIF du fichier original.
+#
 
 # To be implemented.
+
+#
+# \function Import raw images
+#
+# \param $1 Chemin vers le répertoire où est monté la carte de l'appareil
+#           photo.
+# \param $2 Chemin vers le répertoire où les images brutes de la carte de
+#           l'appareil photo doivent être copiée.
+#
+function import_CR2()
+{
+    local org_path=$1/DCIM/100EOS5D
+    local dst_path=$2
+    local org_file
+    local counter=1
+    
+    for org_file in ${org_path}/*.CR2
+    do
+	local directory=$(exiftool ${org_file} | awk '/File Modification Date/ { print $5 }' | tr ':' '/')
+	local file=$(basename ${org_file})
+
+	[[ -d ${dst_path}/${directory} ]] || mkdir -p ${dst_path}/${directory}
+	if [[ ! -f ${dst_path}/${directory}/${file} ]]
+	then
+	    md5sum ${org_file} > ${dst_path}/${directory}/${file}.md5
+	    sed -i "s|${org_file}|${dst_path}/${directory}/${file}|" ${dst_path}/${directory}/${file}.md5
+	    
+	    cp ${org_file} ${dst_path}/${directory}/${file}
+	fi
+    done
+    
+    return 0
+}
+
+#
+# Deux arguments sont nécessaires au script. A compléter plus tard par un
+# usage du script et des vérifications sur les paramètres passés.
+# 
+[[ $# -eq 2 ]] || exit 1
+
+#
+# Import des images brutes.
+#
+import_CR2 $1 $2
 
 exit 0
